39af58a28fd5dbbb85f051a8726d8f3f91e35691
Leo Muckley <leo_muckley@outlook.com>
2020-07-08 19:34:26 +0100

    Updated for Malawi images

---
+++ gee_images.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 25 16:27:42 2020
+
+@author: leo
+"""
+
+import geopandas as gpd
+import rasterio
+from rasterstats import zonal_stats
+from urllib.parse import urljoin
+import pandas as pd
+
+class GeeImages():
+    
+    def __init__(self, filepath, filelist):
+        self.filepath = filepath
+        self.filelist = [urljoin(self.filepath, i) for i in filelist]      
+
+
+    def get_stats(self, df, stats_list, return_stats='mean'):
+        s = []
+        for i in range(len(df)):
+            s.append(stats_list[i][return_stats])
+                         
+        return pd.Series(s)
+
+        
+    def add_image(self, df, file):
+        image = rasterio.open(file)
+        band1 = image.read(1)
+        affine = image.transform
+        gdf = gpd.GeoDataFrame(df)
+        
+        stat = zonal_stats(gdf, band1, affine=affine, stats=['mean'])
+        file = file.replace('.tif', '')
+        file = file.replace(f'{self.filepath}', '')
+        df[f'{file}'] = self.get_stats(df, stats_list=stat, return_stats='mean')
+        
+        # Return new df
+        return df
+    
+        
+    def add_all_images(self, df):
+        
+        for file in self.filelist:
+            df = self.add_image(df, file)
+            
+        return df
+        
--- lstm-v1.py
+++ lstm-v1.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+    #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Sun Jun 14 17:09:48 2020
@@ -72,6 +72,11 @@ import matplotlib.pyplot as plt
 # rmse(preds, y_test)
 
 
+"""
+
+Try using max over a three-day period.
+
+"""
 
 
 # Import Libraries
@@ -79,93 +84,128 @@ import numpy as np
 import pandas as pd
 from scipy import stats
 import lightgbm as lgb
-
-from bayes_opt import BayesianOptimization
-
-# Seed to be used for al models etc.
-SEED = 400
-
-df1 = pd.read_csv("moz_orig_df.csv")
-
-df2 = pd.read_csv("moz_aug_df.csv")
-
-df3 = pd.read_csv("moz_diam_df.csv")
-
-
-train_df = pd.concat([df1, df2])
-train_df = pd.concat([train_df, df3])
-
-
-train_df = train_df[train_df['target'] > 0.0]
-
-y = train_df['target']
-X = train_df.drop('target', axis=1)
-
-
-
-def preprocess_data(X, y):
-        
-    # Take log of skewed target
-    y = np.log1p(y)
-    
-    # Compute Z-score to remove outliers
-    z = np.abs(stats.zscore(y))
-    #print(z)   
-    threshold = 2.5
-    idx = np.where(z < threshold)[0]
-    y = y.iloc[idx]
-    X = X.iloc[idx, :]
-    
-    return X, y
-
-
-X, y = preprocess_data(X, y)
-
-
-
-X_train = X[:8000]
-X_test = X[8000:]
-
-
-# X = data.iloc[:, :16]
-# y = data['precip_wk-1']
 from sklearn.preprocessing import MinMaxScaler
 
-scaler = MinMaxScaler()
-
-# You normalize the last bit of remaining data
-scaler.fit(X)
-X = scaler.transform(X)
-
-y_train = y[:8000]
-y_test = y[8000:]
 
 # Example of one output for whole sequence
 from keras.models import Sequential
 from keras.layers import LSTM, Dense, Dropout
 import numpy as np
 
+
+
+from sklearn.covariance import EllipticEnvelope
+
+
+# Seed to be used for al models etc.
+SEED = 400
+
+df = pd.read_csv("data/moz_orig_df.csv")
+
+df1 = pd.read_csv("data/moz_aug_df1.csv")
+
+df2 = pd.read_csv("data/moz_aug_df2.csv")
+
+df3 = pd.read_csv("data/moz_aug_df3.csv")
+
+
+train_df = pd.concat([df, df1])
+train_df = pd.concat([train_df, df2])
+train_df = pd.concat([train_df, df3])
+
+
+# train_df = train_df[train_df['target'] > 0.0]
+
+#feat = [i for i in train_df.columns if "susm" not in i]
+
+#train_df = train_df[feat]
+
+train_df = train_df.fillna(train_df.mean())
+
+
+clf2 = EllipticEnvelope(contamination=.20,random_state=SEED)
+clf2.fit(train_df)
+
+ee_scores = pd.Series(clf2.decision_function(train_df))
+clusters2 = clf2.predict(train_df)
+
+train_df['pred'] = clusters2
+train_df = train_df[train_df['pred'] != -1]
+X, y = train_df.drop(columns=['pred','target']), train_df['target']
+
+
+# y = train_df['target']
+# X = train_df.drop('target', axis=1)
+
+feat = [i for i in X.columns if "susm" not in i]
+X = X[feat]
+
+
+# def preprocess_data(X, y):
+        
+#     # Take log of skewed target
+#     y = np.log1p(y)
+    
+#     # Compute Z-score to remove outliers
+#     z = np.abs(stats.zscore(y))
+#     #print(z)   
+#     threshold = 2.5
+#     idx = np.where(z < threshold)[0]
+#     y = y.iloc[idx]
+#     X = X.iloc[idx, :]
+    
+#     return X, y
+
+
+# X, y = preprocess_data(X, y)
+
+
+
+# X_train = X[:27500]
+# X_test = X[27500:]
+
+
+scaler = MinMaxScaler(feature_range=(0,1))
+
+import tensorflow as tf
+
+# def get_huber_loss_fn(**huber_loss_kwargs):
+#     """ Keras wrapper for loss function. """
+# def custom_huber_loss(y_true, y_pred):
+#     return tf.keras.losses.Huber(y_true, y_pred)
+    # return custom_huber_loss
+
+callback = tf.keras.callbacks.EarlyStopping(monitor='val_root_mean_squared_error', 
+                                            restore_best_weights=True, min_delta=0.0001, patience=2)
+
+
+
 # define model where LSTM is also output layer
-model = Sequential()
-model.add(LSTM(1, return_sequences=True, input_shape=(30,1)))
-model.add(LSTM(1, dropout=0.1, recurrent_dropout=0.1))
-model.add(Dense(1))
-model.compile(optimizer='adam', loss='mse')
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.LSTM(64, return_sequences=True, input_shape=(30,5)))
+model.add(tf.keras.layers.LSTM(32, dropout=0.1, recurrent_dropout=0.1))
+model.add(tf.keras.layers.Dense(1))
+#model.compile(optimizer='adam', loss='mse', metrics=[tf.keras.metrics.RootMeanSquaredError()])
+model.compile(optimizer='adam', loss=tf.keras.losses.Huber(), metrics=[tf.keras.metrics.RootMeanSquaredError()])
 
-X_train = X_train.values.reshape((len(X_train), 30, 1))
+# X_train = X_train.reshape((len(X_train), 30, 4))
 
-model.fit(X_train, y_train, epochs=5)
+# model.fit(X_train, y_train, epochs=5)
 
-X_test = X_test.values.reshape((len(X_test), 30, 1))
+# X_test = scaler.fit_transform(X_test)
+# X_test = X_test.reshape((len(X_test), 30, 1))
 
 
-preds = model.predict(X_test).reshape(len(X_test,))
+
+# preds = model.predict(X_test).reshape(len(X_test,))
 
 
 def rmse(predictions, targets):
+    predictions = np.float32(predictions)
+    targets = np.float32(targets)
     return np.sqrt(((predictions - targets) ** 2).mean())
 
-rmse(preds, y_test)
+# rmse(preds, y_test)
 
 
 
@@ -177,28 +217,124 @@ yy = pd.qcut(y, 10, labels=False, duplicates='drop')
 
 
 from sklearn.model_selection import StratifiedKFold
-skf = StratifiedKFold(n_splits=5)
+skf = StratifiedKFold(n_splits=5, random_state=SEED)
 skf.get_n_splits(X, yy)
 
 X = pd.DataFrame(X)
 #print(skf)
 count = 1
-for train_index, test_index in skf.split(X, yy):
-    print("TRAIN:", train_index, "TEST:", test_index)
-    X_train, X_test = X.iloc[train_index, :], X.iloc[test_index, :]
-    y_train, y_test = y.iloc[train_index], y.iloc[test_index]
-    X_train = X_train.values.reshape((len(X_train), 30, 1))
-    model.fit(X_train, y_train, epochs=5)
-    X_test = X_test.values.reshape((len(X_test), 30, 1))
-    preds = model.predict(X_test).reshape(len(X_test,))
-    print(f"The RMSE on fold {count} is {rmse(preds, y_test)}")
+rmse_list = []
+pred_list = []
+for train_index, val_index in skf.split(X, yy):
+    print("TRAIN:", train_index, "TEST:", val_index)
+    X_train, X_val = X.iloc[train_index, :], X.iloc[val_index, :]
+    y_train, y_val = y.iloc[train_index], y.iloc[val_index]
+    #y_log_train = np.log1p(y_train)
+    X_train = scaler.fit_transform(X_train)
+    X_val = scaler.fit_transform(X_val)
+    X_train = X_train.reshape((len(X_train), 30, 5))
+    X_val = X_val.reshape((len(X_val), 30, 5))
+    model.fit(X_train, y_train, epochs=10, validation_data=(X_val, y_val),
+                 callbacks=[callback])
+    #model.fit(X_train, y_log_train, epochs=5)
+    preds = model.predict(X_val).reshape(len(X_val,))
+    res = rmse(np.float32(preds), np.float32(y_val))
+    pred_list.append(preds)
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    fig.suptitle(f'RMSE is {res} on Fold {count}')
+    ax1.hist(preds, bins=100)
+    ax1.set_title("Predicted values")
+    ax2.hist(y_val, bins=100)
+    ax2.set_title("True values")
+    plt.show()
+    rmse_list.append(res)
+    # print(f"The RMSE on fold {count} is {res}")
+    count += 1
+    
+print(f"The average RMSE is {np.mean(rmse_list)}")
+
+# The average RMSE is 0.14830604195594788 (with 1 node)
+# The average RMSE is 0.13499458134174347 (with 4 nodes)
+# The average RMSE is 0.12377201020717621 (with 64, 32)
+# The average RMSE is 0.12217734754085541 (huber + validation)
+# The average RMSE is 0.11980126798152924 (+ slope)
+# The average RMSE is 0.11943419277667999 (without outliers 20%)
+
+test_df = pd.read_csv('data/mwi_orig_df.csv')
+
+y_test = test_df['target']
+X_test = test_df.drop('target', axis=1)
+
+X_test = scaler.fit_transform(X_test)
+
+
+X_test = X_test.reshape((len(X_test), 30, 5))
+
+test_preds = model.predict(X_test)
+
+
+test_preds[test_preds < 0.0] = 0.0
+test_preds[test_preds > 1.0] = 1.0
+
+final_preds = pd.Series(test_preds.reshape(len(test_preds),))
+
+test_res = rmse(np.float32(final_preds), np.float32(y_test))
+
+
+def plot_flood(res, rows, cols):
+    
+    flood_list = list(res)
+    mat = np.array(flood_list).reshape(cols, rows)
+    plt.imshow(mat.T) #, cmap='Blues')
+    return plt.show()
+
+
+def plot_flood_preds(res, rows, cols):
+    
+    flood_list = list(res)
+    mat = np.array(flood_list).reshape(rows, cols)
+    plt.imshow(mat) #, cmap='Blues')
+    return plt.show()
+
+plot_flood(final_preds, rows=363, cols=142)
 
 
 
+plot_flood(y_test, rows=363, cols=142)
 
+
+
+#=================================================================
+
+
+
+ids_list = []
+for x, y in skf.split(X, yy):
+    ids_list.append((x, y))
     
 
+# Fold 1:
+corr1 = X.iloc[ids_list[0][0]].corr()
+plt.imshow(corr1)
+plt.show()
+
+# Fold 2:
+corr2 = X.iloc[ids_list[1][0]].corr()
+plt.imshow(corr2)
+plt.show()
 
 
+# Fold 3:
+corr3 = X.iloc[ids_list[2][0]].corr()
+plt.imshow(corr3)
+plt.show()
 
+# Fold 4:
+corr4 = X.iloc[ids_list[3][0]].corr()
+plt.imshow(corr4)
+plt.show()
 
+# Fold 5:
+corr5 = X.iloc[ids_list[4][0]].corr()
+plt.imshow(corr5)
+plt.show()
